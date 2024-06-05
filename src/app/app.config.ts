b2a77e6fbd18e851729550a9ddca2edb87bda41f
hoangnm <hoangnm.ndm@gmail.com>
2024-06-05 13:45:10 +0700

    delete service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { CurrencyPipe } from '@angular/common';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withFetch()),
-    CurrencyPipe,
-  ],
+  providers: [provideRouter(routes), provideHttpClient(withFetch())],
 };
